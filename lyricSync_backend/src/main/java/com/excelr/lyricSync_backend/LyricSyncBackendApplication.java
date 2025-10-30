d7d0582d7b444a2e88a19597adc49159bb714f7c
Excelr Solutions <mailmeasradha@gmail.com>
2025-10-30 13:45:33 +0530

    uploading backend project

---
+++ lyricSync_backend/src/main/java/com/excelr/lyricSync_backend/LyricSyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.excelr.lyricSync_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricSyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricSyncBackendApplication.class, args);
+	}
+
+}
---
+++ lyricSync_backend/src/test/java/com/excelr/lyricSync_backend/LyricSyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excelr.lyricSync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricSyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
